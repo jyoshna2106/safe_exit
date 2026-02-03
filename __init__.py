9893df1d70586532bc2aa74fb5225d566751b711
jyoshna2106 <jyoshnajyo199@gmail.com>
2026-02-03 18:13:47 +0545

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
-from .serving import run_simple as run_simple
-from .test import Client as Client
-from .wrappers import Request as Request
-from .wrappers import Response as Response
+from typing import List, Optional
+
+__version__ = "24.0"
+
+
+def main(args: Optional[List[str]] = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
